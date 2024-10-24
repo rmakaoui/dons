355499ffe9b3606e98dfa0ba9adc34e8941a22fe
reda <redamakaoui2001@gmail.com>
2024-10-24 08:58:34 +0100

    first comm

---
+++ src/main/java/com/isima/dons/DonsApplication.java
@@ -0,0 +1,13 @@
+package com.isima.dons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isima/dons/DonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isima.dons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
