17d0756ec9350769e8ff76ef52aa0f2e1a645998
reda <redamakaoui2001@gmail.com>
2024-10-24 09:14:37 +0100

    first push

---
+++ src/main/java/com/isima/dons/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.isima.dons.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("welcomeMessage", "Welcome to Thymeleaf with Spring Boot!");
+        return "home"; // This returns the template file home.html
+    }
+}
+
